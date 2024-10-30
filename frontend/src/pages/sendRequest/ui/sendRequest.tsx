0c71582d63fa04906ba1e720ffe642b3fa3a2b4f
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-10-30 14:20:06 +0200

    upd

--- frontend/src/pages/sendRequest/ui/sendRequest.tsx
+++ frontend/src/pages/sendRequest/ui/sendRequest.tsx
@@ -6,11 +6,24 @@ export const SendRequest = () => {
       <h1 className={styles.title}>Заполните форму</h1>
       <div className={styles.input_container}>
         <label htmlFor="name">Как вас зовут (ФИО)</label>
-        <input type="text" />
+        <input className={styles.input} type="text" />
       </div>
       <div className={styles.input_container}>
-        <label htmlFor="name">Как вас зовут (ФИО)</label>
-        <input type="text" />
+        <label htmlFor="age">Возраст</label>
+        <input className={styles.input} type="number" />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="city">Город</label>
+        <input className={styles.input} type="text" />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="city">Опыт работы</label>
+        <input className={styles.big_input} type="text" />
+      </div>
+
+      <div className={styles.input_container}>
+        <label htmlFor="resume">Резюме</label>
+        <input className={styles.resume} type="text" />
       </div>
     </>
   );
