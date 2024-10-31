c64e266dac9b75626c85e5d224add9c00bd6aa94
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2024-10-31 13:03:48 +0200

    add load page

--- backend/src/web/app/utils/parsing_vacancies.py
+++ backend/src/web/app/utils/parsing_vacancies.py
@@ -5,14 +5,12 @@ from bs4 import BeautifulSoup
 # def get_vacancies() -> list[str]:
 url = 'https://career.kode.ru/career/'
 response = httpx.get(url, verify=False)
-
 if response.status_code == 200:
-		soup = BeautifulSoup(response.text, 'html.parser')
-		headings = soup.find_all('a')
-		for heading in headings:
-			print(heading.text)
+		soup = BeautifulSoup(response.text, 'html')
+		print(soup)
+		vacancies_div = soup.find('h2', 'sc-fzoxnE eTCim')
+		print(vacancies_div)
 else:
 		print('Ошибка при запросе:', response.status_code)
 
 # get_vacancies()
-print(response.status_code)
